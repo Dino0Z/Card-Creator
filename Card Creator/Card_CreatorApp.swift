ac72a23f04fa8db2f8900c8fae39144cc8ac94f8
Dino0Z <andre.jansson7@gmail.com>
2023-03-15 19:50:08 +0100

    Initial Commit

---
+++ Card Creator/Card_CreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card_CreatorApp.swift
+//  Card Creator
+//
+//  Created by Dino0Z on 2023-03-15.
+//
+
+import SwiftUI
+
+@main
+struct Card_CreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
