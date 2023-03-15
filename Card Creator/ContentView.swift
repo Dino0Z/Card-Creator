1bab10c605e3a49468280b00ed6ce0b046b42297
Dino0Z <andre.jansson7@gmail.com>
2023-03-15 21:42:53 +0100

    Updated app with buttons

--- Card Creator/ContentView.swift
+++ Card Creator/ContentView.swift
@@ -8,12 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showDetails = false
+    @State private var writtenText = "You went to "
+    @State private var writtenDestination = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button() {
+                showDetails.toggle()
+            } label: {
+                Text("Go home")
+                Image(systemName: "house")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+            }
+            .buttonStyle(.bordered)
+            Button() {
+                showDetails.toggle()
+               let writtenDestination = "Space"
+            } label: {
+                Text("Go to space")
+                Image(systemName: "aqi.medium")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+            }
+            .buttonStyle(.bordered)
+            VStack {
+                Text("")
+                if showDetails {
+                    Text(writtenText + writtenDestination + "Congratz")
+                }
+                else {
+                    Text("").scaledToFit()
+                    }
+                
+            }
+    
+         
+//            Image(systemName: "camera")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+//            Text("Hello, world!")
         }
         .padding()
     }
